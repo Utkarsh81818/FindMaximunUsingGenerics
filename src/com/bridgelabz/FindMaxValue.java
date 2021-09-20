c4be1cb216efcdc4b2761e029d36e3078d02e2ed
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-20 14:24:37 +0530

    Welcome Message

---
+++ src/com/bridgelabz/FindMaxValue.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class FindMaxValue {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Find Maximun Problem Using Generics");
+    }
+}
