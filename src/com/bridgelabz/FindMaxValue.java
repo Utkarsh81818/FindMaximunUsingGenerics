6e131e2c4642efffb7f527d66f5c082c5d7bd07f
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-20 15:14:06 +0530

    Added: Finding maximum between three Integer

--- src/com/bridgelabz/FindMaxValue.java
+++ src/com/bridgelabz/FindMaxValue.java
@@ -1,8 +1,30 @@
 package com.bridgelabz;
-
+/**
+ * Purpose - To find maximum integer value using generics
+ * @author - Utkarsh Mishra
+ * @since - 2021-09-20
+ */
 public class FindMaxValue {
 
+    /**
+     * Method used to compare three integer values and get the maximum value.
+     * @param a1 : First value to compare
+     * @param a2 : Second value to compare
+     * @param a3 : Third value to compare
+     * @return max : Maximum of three integers
+     */
+
+    public static Integer maxOfInteger(Integer a1, Integer a2, Integer a3){
+        Integer max = a1;
+        if (a2.compareTo(max) > 0)
+            max = a2;
+        if (a3.compareTo(max) > 0)
+            max = a3;
+        return max;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome To Find Maximun Problem Using Generics");
+        System.out.println("Welcome To Find Maximun Problem Using Generics \n");
+        System.out.println("The maximum value between the three integer is : " + maxOfInteger(3, 8, 5));
     }
 }
